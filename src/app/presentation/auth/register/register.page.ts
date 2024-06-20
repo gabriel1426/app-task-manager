efcb370623ab2659c4a4d8fdd7af3ca9053c8bc2
Melvy Martinez <melvy_martinez@bhdleon.com.do>
2024-06-20 17:07:44 -0400

    feat: subiendo avances visuales de pantalla de registro

--- src/app/presentation/auth/register/register.page.ts
+++ src/app/presentation/auth/register/register.page.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+// import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonIcon } from '@ionic/angular/standalone';
 import { UserRegisterModel } from '@app/modules/auth/register/domain/user-register.model';
 import { CaseRegisterService } from '@app/modules/auth/register/application/user-cases/case-register/case-register.service';
 import { UserRegisterRepository } from '@app/modules/auth/register/domain/user-register.repository';
 import { UserRegisterImplService } from '@app/modules/auth/register/insfraestructure/driven-adapter/user-register-impl.service';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -19,13 +20,12 @@ import { UserRegisterImplService } from '@app/modules/auth/register/insfraestruc
       useClass: UserRegisterImplService
     }
   ],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, IonicModule, FormsModule]
 })
 export class RegisterPage {
   private caseRegisterService = inject(CaseRegisterService);
 
   constructor() {
-    this.register();
   }
 
   async register() {

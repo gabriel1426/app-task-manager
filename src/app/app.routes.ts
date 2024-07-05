79c2d761e49e6b85dacac0a685df8e2cdc5d4e0a
Melvy Martinez <melvy_martinez@bhdleon.com.do>
2024-07-05 13:10:17 -0400

    fix: ajustando el route para enviar al modulo de auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'user', // TODO: colocar nuevamente el path 'auth'
+    redirectTo: 'auth',
     pathMatch: 'full'
   },
   {
